0833e79c685505cf7700a1012d7175bc094216ce
Jamal Frank <jamaljfrank@gmail.com>
2020-01-19 09:38:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", NULL, NULL, NULL, NULL, NULL);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", NULL, NULL, NULL, NULL, NULL);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", NULL, NULL, NULL, NULL, NULL);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6);
